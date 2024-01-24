efdf3c3ca6609d2c839e1061edee6f466e989767
NA <NA@NA.com>
2024-01-24 15:02:52 +0000

    Basic console interface and library functions complete.
    Time to actully get to work.

---
+++ src/binary.rs
@@ -0,0 +1,102 @@
+use std::{env, fs, path::{Path, PathBuf}, process::exit};
+
+use clap::{Parser, Subcommand};
+use url::Url;
+
+#[derive(clap::Parser, Debug)]
+#[command(version)]
+struct Cli {
+    #[command(subcommand)]
+    subcommand: Subcommands,
+
+    /// Enter the URL of the Webnovel
+    book_url: String,
+
+    /// Enter the output directory for the generated format.
+    /// Leave blank to use current directory.
+    output_directory: Option<String>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Subcommands {
+    /// Generate an Audiobook from the webnovel.
+    /// 'audiobook --help' for available arguments.
+    Audiobook(royal_road_archiver_lib::AudiobookArgs),
+
+    /// Generate an epub from the Webnovel.
+    /// 'epub --help' for available arguments.
+    Epub(royal_road_archiver_lib::EpubArgs),
+
+    /// Store the webnovel as a collection of HTML pages.
+    /// 'html --help' for available arguments.
+    Html(royal_road_archiver_lib::HtmlArgs),
+
+    /// Generate a markdown file from the Webnovel.
+    /// 'markdown --help' for available arguments.
+    Markdown(royal_road_archiver_lib::MarkdownArgs),
+}
+
+fn main() {
+    let cli_input = Cli::parse();
+
+    // Turn the inputted string into a path, or grab the current directory if empty.
+    let output_directory: PathBuf;
+    match cli_input.output_directory {
+        Some(output_directory_input) => {
+            output_directory = Path::new(&output_directory_input).to_path_buf();
+        },
+        None => {
+            output_directory = env::current_dir().unwrap().as_path().to_path_buf();
+        }
+    }
+
+    valid_directory_check(&output_directory);
+    let book_url = valid_url_check(&cli_input.book_url.to_lowercase());
+
+    match cli_input.subcommand {
+        Subcommands::Audiobook(audiobook_args) => royal_road_archiver_lib::generate_audiobook(audiobook_args, book_url, output_directory),
+        Subcommands::Epub(epub_args) => royal_road_archiver_lib::generate_epub(epub_args, book_url, output_directory),
+        Subcommands::Html(html_args) => royal_road_archiver_lib::generate_html(html_args, book_url, output_directory),
+        Subcommands::Markdown(markdown_args) => royal_road_archiver_lib::generate_markdown(markdown_args, book_url, output_directory),
+    }
+}
+
+// Check if the directory exists and is writeable. Creates one if not.
+fn valid_directory_check(output_directory: &Path) {
+    // Check if the directory exists, if it does not; attempt to create one.
+    if !output_directory.exists() {
+        match fs::create_dir_all(output_directory) {
+            Ok(_) => (),
+            Err(error) => {
+                eprintln!("Error! Unable to create directory: {error}");
+                exit(1);
+            }
+        }
+    }
+
+    // Check if the user has write permissions:
+    if fs::metadata(output_directory).unwrap().permissions().readonly() {
+        eprintln!("Error! You do not have permissions for the specified directory.");
+        exit(1);
+    }
+}
+
+// Check if the given URL is a valid royalroad url.
+fn valid_url_check(book_url: &str) -> Url {
+    match Url::parse(book_url) {
+        Ok(book_url) => {
+
+            if book_url.host_str() == Some("www.royalroad.com") {
+                return book_url;
+            }
+            else {
+                eprintln!("Error! Please enter a RoyalRoad URL.");
+                exit(1);
+            }
+        },
+        Err(error) => {
+            eprintln!("Error! Unable to parse url: {error}");
+            exit(1);
+        }
+    }
+}
---
+++ src/library.rs
@@ -0,0 +1,71 @@
+use std::path::PathBuf;
+
+use clap::Args;
+use url::Url;
+
+#[derive(Args, Debug)]
+pub struct AudiobookArgs {
+    /// Disable the generation of chapter titles in the audio file. Useful to avoid chapter titles appearing twice.
+    #[arg(short, long)]
+    pub no_chapter_titles: bool,
+
+    /// Split the novel into multiple audio files by chapter.
+    #[arg(short, long)]
+    pub split_novel_by_chapters: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct EpubArgs {
+    /// Disable the inclusion of images.
+    /// Will speed up epub generation and significantly decrease epub size.
+    #[arg(short, long)]
+    pub no_images: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct HtmlArgs {
+
+}
+
+#[derive(Args, Debug)]
+pub struct MarkdownArgs {
+    /// Disable the generation of chapter titles. Useful to avoid chapter titles appearing twice.
+    #[arg(short, long)]
+    pub no_chapter_titles: bool,
+
+    /// Disables the inclusion of html image tags in the markdown.
+    #[arg(short='i', long)]
+    pub no_image_tags: bool,
+}
+
+/// Generate an audiobook from the given arguments, url, & outputs it to the output directory.
+/// 
+/// This function DOES NOT do any error checking on the Url or output directory & WILL panic if they are wrong. 
+/// Make sure the Url is valid and the output directory is writable BEFORE passing them to this.
+pub fn generate_audiobook(audiobook_args: AudiobookArgs, book_url: Url, output_directory: PathBuf) {
+    eprintln!("This is not implemented yet.");
+}
+
+/// Generate an epub file from the given arguments, url, & outputs it to the output directory.
+/// 
+/// This function DOES NOT do any error checking on the Url or output directory & WILL panic if they are wrong. 
+/// Make sure the Url is valid and the output directory is writable BEFORE passing them to this.
+pub fn generate_epub(epub_args: EpubArgs, book_url: Url, output_directory: PathBuf) {
+    eprintln!("This is not implemented yet.");
+}
+
+/// Generate an html archive from the given arguments, url, & outputs it to the output directory.
+/// 
+/// This function DOES NOT do any error checking on the Url or output directory & WILL panic if they are wrong. 
+/// Make sure the Url is valid and the output directory is writable BEFORE passing them to this.
+pub fn generate_html(html_args: HtmlArgs, book_url: Url, output_directory: PathBuf) {
+    eprintln!("This is not implemented yet.");
+}
+
+/// Generate a markdown file from the given arguments, url, & outputs it to the output directory.
+/// 
+/// This function DOES NOT do any error checking on the Url or output directory & WILL panic if they are wrong. 
+/// Make sure the Url is valid and the output directory is writable BEFORE passing them to this.
+pub fn generate_markdown(markdown_args: MarkdownArgs, book_url: Url, output_directory: PathBuf) {
+    eprintln!("This is not implemented yet.");
+}
